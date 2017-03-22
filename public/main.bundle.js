f0e56d1febefd0be64cdc5685e4bfccaec871294
Bikram Baral <baralbikram@hotmail.com>
2017-03-22 15:33:05 -0700

    node js

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -713,38 +713,25 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 var AuthService = (function () {
+    //isDev: boolean;
     function AuthService(http) {
         this.http = http;
-        this.isDev = false; //change to false before deployment
+        //this.isDev=false; //change to false before deployment
     }
     //Register User post method
-    // registerUser(user) {
-    //   let headers = new Headers();
-    //   headers.append('Content.Type', 'application/json');
-    //   //return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
-    //   return this.http.post('users/register', user, {headers: headers})
-    //
-    //     .map(res => res.json());
     AuthService.prototype.registerUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
-        headers.append('Content-Type', 'application/json');
-        var ep = this.prepEndpoint('users/register');
-        return this.http.post(ep, user, { headers: headers })
+        headers.append('Content.Type', 'application/json');
+        //return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+        return this.http.post('users/register', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     //Authenticate User Post Method
-    // authenticateUser(user) {
-    //   let headers = new Headers();
-    //   headers.append('Content.Type', 'application/json');
-    //   // return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
-    //   return this.http.post('users/authenticate', user, {headers: headers})
-    //
-    //     .map(res => res.json());
     AuthService.prototype.authenticateUser = function (user) {
         var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]();
-        headers.append('Content-Type', 'application/json');
-        var ep = this.prepEndpoint('users/authenticate');
-        return this.http.post(ep, user, { headers: headers })
+        headers.append('Content.Type', 'application/json');
+        // return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+        return this.http.post('users/authenticate', user, { headers: headers })
             .map(function (res) { return res.json(); });
     };
     //get profile
@@ -753,7 +740,7 @@ var AuthService = (function () {
         this.loadToken();
         headers.append('Authorization', this.authToken);
         headers.append('Content-Type', 'application/json');
-        var ep = this.prepEndpoint('users/profile');
+        //let ep = this.prepEndpoint('users/profile');
         //return this.http.get(ep, {headers: headers})
         // return this.http.get('http://localhost:3000/users/profile', {headers: headers})
         return this.http.get('users/profile', { headers: headers })
@@ -783,12 +770,10 @@ var AuthService = (function () {
     };
     //PrepEnd point
     AuthService.prototype.prepEndpoint = function (ep) {
-        if (this.isDev) {
-            return ep;
-        }
-        else {
-            return 'http://localhost:8080/' + ep;
-        }
+        // if (this.isDev) {
+        // return ep;
+        //} else {
+        // return 'http://localhost:8080/' + ep;
     };
     AuthService = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(), 
